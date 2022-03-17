1364ee27ca50a4860f82689a2539f184d19839ac
ben <bjelinek42@gmail.com>
2022-03-17 16:52:20 -0600

    validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validation :title, length: { minimum: 2}
+  validation :director, length: { minimum: 2}
+  validation :year, length: { minimum: 4}
+  validation :plot, length: { minimum: 10}
+  validation :plot, length: { maximum: 100}
 end
